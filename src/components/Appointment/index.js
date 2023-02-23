7d981798acc4a2627ca5a62edcf13f94de0fc964
woodenonesie <olga.pi4ugina@yandex.ru>
2023-02-23 05:54:14 -0800

    add list of appointments on the site

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React from 'react';
 import "./styles.scss";
+import Header from "./Header.js";
+import Show from "./Show.js";
+import Empty from "./Empty.js";
+
 
 export default function Appointment(props) {
+  const interview = props.interview;
+
   return (
-  <article className="appointment"></article>
+  <article className="appointment">
+    <Header time={props.time}/>
+    {props.interview ? <Show student={interview.student} interviewer={interview.interviewer}/> : <Empty />}
+  </article>
   )
 }
