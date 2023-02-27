985044f6e4b81e8dc2cdf05a8af5e5d08fb42dbb
woodenonesie <olga.pi4ugina@yandex.ru>
2023-02-27 05:06:39 -0800

    add modes for creating forms

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,15 +3,33 @@ import "./styles.scss";
 import Header from "./Header.js";
 import Show from "./Show.js";
 import Empty from "./Empty.js";
+import Form from "./Form.js"
+import useVisualMode from "../../hooks/useVisualMode.js"
 
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
-  const interview = props.interview;
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+    console.log(props)
 
   return (
-  <article className="appointment">
-    <Header time={props.time}/>
-    {props.interview ? <Show student={interview.student} interviewer={interview.interviewer}/> : <Empty />}
-  </article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && <Form
+        interviewers={[]}
+        onCancel={() => back()}
+      />}
+    </article>
   )
 }
