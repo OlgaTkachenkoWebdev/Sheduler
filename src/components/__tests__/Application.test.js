3114ee593aa89a8a78d9f7c5d7ac208d7c2a6609
woodenonesie <olga.pi4ugina@yandex.ru>
2023-03-02 19:52:28 -0800

    add default to monday to the first test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -9,12 +9,13 @@ afterEach(cleanup);
 
 describe("Application", () => {
 
-  it("changes the schedule when a new day is selected", async () => {
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
     const { getByText } = render(<Application />);
-
-    await waitForElement(() => getByText("Monday"));
-    fireEvent.click(getByText("Tuesday"));
-    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  
+    return waitForElement(() => getByText("Monday")).then(() => {
+      fireEvent.click(getByText("Tuesday"));
+      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    });
   });
 
   it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
@@ -123,7 +124,7 @@ describe("Application", () => {
   it("shows the delete error when failing to delete an existing appointment", async () => {
     axios.delete.mockRejectedValueOnce("Could not cancel appointment");
 
-    const { container, debug } = render(<Application />);
+    const { container} = render(<Application />);
 
     await waitForElement(() => getByText(container, "Archie Cohen"));
 
@@ -141,4 +142,60 @@ describe("Application", () => {
     fireEvent.click(getByAltText(appointment, "Close"));
     expect(appointment)
   });
+
+  it("loads data, starts to book an interview and cancels it without saving", async () => {
+    const { container } = render(<Application />);
+
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[0];
+
+    fireEvent.click(getByAltText(appointment, "Add"));
+
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+    fireEvent.click(getByText(appointment, "Cancel"));
+
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+  });
+
+  it("loads data, edits an interview and cancels without saving", async () => {
+    const { container } = render(<Application />);
+
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    const appointment = getAllByTestId(container, "appointment").find(
+      appointment => queryByText(appointment, "Archie Cohen")
+    );
+    fireEvent.click(queryByAltText(appointment, "Edit"));
+    fireEvent.change(getByPlaceholderText(appointment, "Archie Cohen"), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+    fireEvent.click(getByText(appointment, "Cancel"));
+
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+  });
+
+  it("loads data, cancels an interview and cancels confirmation of edleting", async () => {
+    const { container } = render(<Application />);
+
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    const appointment = getAllByTestId(container, "appointment").find(
+      appointment => queryByText(appointment, "Archie Cohen")
+    );
+
+    fireEvent.click(queryByAltText(appointment, "Delete"));
+    expect(getByText(appointment, "Do you want to delete this appointment?")).toBeInTheDocument();
+
+    fireEvent.click(getByText(appointment, "Cancel"));
+
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+  })
 })
